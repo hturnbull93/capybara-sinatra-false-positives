c723d100db8ac17821c7177d4780db0732de399c
Harry Turnbull <hturnbull@live.co.uk>
2020-04-24 21:59:42 +0100

    third way of avoiding false positive

--- spec/features/echo_spec.rb
+++ spec/features/echo_spec.rb
@@ -25,4 +25,12 @@ feature 'Echo' do
       expect(page).to have_content 'Hello'
     }
   end
+
+  scenario 'Correct Failure: Another way of specifying certain element to contain Hello' do
+    visit '/'
+    expect(page).to have_content 'Echo Generator'
+    fill_in('echo', with: "Hello")
+    click_on 'Shout'
+    expect(page.first('#result')).to have_content 'Hello'
+  end
 end

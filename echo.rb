1f01d9d5ae14445767c48af188a6eb3e4cf3c205
Harry Turnbull <hturnbull@live.co.uk>
2020-04-24 21:33:57 +0100

    Added deliberate mistake

--- echo.rb
+++ echo.rb
@@ -1,9 +1,11 @@
 require 'sinatra/base'
 
 class Echo < Sinatra::Base
-
   get '/' do
     erb :index
   end
 
+  post '/echo' do
+    param['echo'] # A deliberate mistake for demonstration purposes
+  end
 end

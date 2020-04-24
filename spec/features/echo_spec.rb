3d8417bfb8ce15bc6eafc968d3cc4278252291f3
Harry Turnbull <hturnbull@live.co.uk>
2020-04-24 20:40:03 +0100

    added correct test

--- spec/features/echo_spec.rb
+++ spec/features/echo_spec.rb
@@ -1,9 +1,18 @@
 feature 'Echo' do
-  scenario 'entering Hello echos Hello' do
+  scenario 'False Positive: entering Hello echos Hello' do
     visit '/'
     expect(page).to have_content 'Echo Generator'
     fill_in('echo', with: "Hello")
     click_on 'Shout'
     expect(page).to have_content 'Hello'
+  end
+
+  scenario 'Correct Failure: entering Hello echos Hello' do
+    visit '/'
+    expect(page).to have_content 'Echo Generator'
+    fill_in('echo', with: "Hello")
+    click_on 'Shout'
+    expect(page).to have_content 'Hello'
+    expect(page).to_not have_content 'Sinatra'
   end
 end
